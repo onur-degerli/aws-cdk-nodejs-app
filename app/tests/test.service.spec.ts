36db32722aafb3dfdb6a787e5d36c5049e0dbef7
Onur Degerli <onurdegerli@gmail.com>
2025-10-21 19:57:46 +0200

    Testinf failure on deployment

--- app/tests/test.service.spec.ts
+++ app/tests/test.service.spec.ts
@@ -3,6 +3,6 @@ import { TestService } from '../services/test.service';
 describe('TestService', () => {
   it('should return true from doLogic', () => {
     const service = new TestService();
-    expect(service.doLogic()).toBe(true);
+    expect(service.doLogic()).toBe(false);
   });
 });
