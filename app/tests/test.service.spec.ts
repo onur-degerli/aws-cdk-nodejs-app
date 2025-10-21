fcec0ee4c8ec950a04081a2b8dd0918507116277
Onur Degerli <onurdegerli@gmail.com>
2025-10-21 19:59:28 +0200

    Revert testing failure on deployment

--- app/tests/test.service.spec.ts
+++ app/tests/test.service.spec.ts
@@ -3,6 +3,6 @@ import { TestService } from '../services/test.service';
 describe('TestService', () => {
   it('should return true from doLogic', () => {
     const service = new TestService();
-    expect(service.doLogic()).toBe(false);
+    expect(service.doLogic()).toBe(true);
   });
 });
