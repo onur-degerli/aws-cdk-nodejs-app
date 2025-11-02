aaa958907b8d93e3702168d9172825d755086f83
Onur Degerli <onurdegerli@gmail.com>
2025-11-02 10:54:44 +0100

    Implement prisma

--- app/server.ts
+++ app/server.ts
@@ -1,21 +1,25 @@
 import express, { Request, Response } from 'express';
-import { getDbClient } from './db';
-import { Client } from 'pg';
+// import { getDbClient } from './db';
+// import { Client } from 'pg';
 import { log } from './common/logger';
+import { PrismaClient } from './generated/prisma/client';
 
 const app = express();
-let dbClient: Client;
+// let dbClient: Client;
+const prisma = new PrismaClient();
 
 app.get('/', async (req: Request, res: Response) => {
   try {
-    if (!dbClient) {
+    /* if (!dbClient) {
       dbClient = await getDbClient();
-    }
+    } */
 
-    const result = await dbClient.query('SELECT NOW() as current_time');
+    // const result = await dbClient.query('SELECT NOW() as current_time');
+    const userCount = await prisma.user.count();
     res.json({
       message: 'Hello from TypeScript App Runner!',
-      time: result.rows[0].current_time,
+      userCount: userCount,
+      // time: result.rows[0].current_time,
     });
   } catch (err) {
     log.error(err);
